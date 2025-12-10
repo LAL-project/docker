d1924798f1329fb96703a34a903e1d04df6698f8
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-12-10 15:59:38 +0100

    Updated for specific version 23.01

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -4,9 +4,14 @@ echo "Generating en_GB.UTF-8 locale..."
 locale-gen en_GB.UTF-8
 
 echo "Generating python3 virtual environment..."
-python3.12 -m venv /workspace/.lal
-source .lal/bin/activate
+python3.8 -m venv /workspace/.lal
+source /workspace/.lal/bin/activate
+pip install --upgrade pip
 pip3 install setuptools wheel ipykernel
 
-echo "Compiling LAL"
-./scripts/compile.sh
+echo "Downloading and installing additional software"
+mkdir -p /workspace/software
+/workspace/scripts/install_swig.sh
+
+echo "Downloading and installing LAL"
+/workspace/scripts/install_LAL.sh
